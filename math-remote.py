1509c3d898e5eb55ec571186f0aab39c1a3f753d
gurdeepsingh2 <gurdeep_singh2@persistent.com>
2022-01-31 21:21:12 +0530

    New python file with implementation

---
+++ math-remote.py
@@ -0,0 +1,19 @@
+#Add implementation
+def add(x,y):
+    x+y;
+    return;
+
+#Sutract implementation
+def subtract(x,y):
+    x-y;
+    return;
+
+#Multiply implementation
+def multiply(x,y):
+    x*y;
+    return;
+
+#Divide implementation
+def divide(x,y):
+    x/y;
+    return;
